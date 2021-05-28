b6376a1654b55c9d933fc8ec368135ff416d4c51
hermanu <manuguionbajo@gmail.com>
2021-05-28 19:14:02 +0200

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue 3 Todo App</h1>
+  <form @submit.prevent="addNewTodo">
+    <label for="newTodo"></label>
+    <input v-model="newTodo" type="text" name="newTodo" />
+    <button>Add New Todo</button>
+  </form>
+  <button @click="markAllDone">Mark All Done</button>
+  <button @click="removeAll">Remove all</button>
+  <ul>
+    <li v-for="(todo, index) in todoList" :key="todo.id" class="todo">
+      <h3 :class="{ done: todo.done }" @click="toggleDone(todo)">
+        {{ todo.content }}
+      </h3>
+      <button @click="removeTodo(index)">Remove</button>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    //
+    const newTodo = ref("");
+    const todoList = ref([]);
+    //
+    function addNewTodo() {
+      todoList.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+      newTodo.value = "";
+    }
+    //
+    function toggleDone(todo) {
+      todo.done = !todo.done;
+    }
+    //
+    function removeTodo(index) {
+      todoList.value.splice(index, 1);
+    }
+    //
+    function markAllDone() {
+      todoList.value.forEach((todo) => (todo.done = true));
+    }
+    //
+    function removeAll() {
+      todoList.value = [];
+    }
+    return {
+      addNewTodo,
+      newTodo,
+      todoList,
+      toggleDone,
+      removeTodo,
+      markAllDone,
+      removeAll,
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +71,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.done {
+  text-decoration: line-through;
+}
+.todo {
+  cursor: pointer;
 }
 </style>
